221f8550029ff083fa528de97ed5cd889e637c42
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-15 09:59:36 +0600

    updated

--- src/test/java/testrunner/TestRun.java
+++ src/test/java/testrunner/TestRun.java
@@ -4,6 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
+        //It includes feature files when test steps are defined
         features = "src/test/java/featuresfiles/thirdScenario.feature",
         glue = "step_definition"
 //        plugin={"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",

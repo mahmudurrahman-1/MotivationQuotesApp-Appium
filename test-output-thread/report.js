46d2e6ef8861214d18f12e3f87c8b6525ac94537
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-15 09:48:10 +0600

    updated

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"2ae56936-07ea-457e-b351-26d38b090c34","feature":"Quotes Feature","scenario":"Sujon selects the given image and perform actions","start":1663131053515,"group":1,"content":"","tags":"","end":1663131221248,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"b49533bd-5644-4a01-b83f-e6887c0575d0","feature":"Menubar Feature","scenario":"Validation of manubar","start":1663213610351,"group":1,"content":"","tags":"","end":1663213657543,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });

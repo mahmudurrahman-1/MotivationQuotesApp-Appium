c15484a09477871dd05078a893925c996db027d9
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-15 09:53:46 +0600

    updated

--- test-output-thread/report.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"b49533bd-5644-4a01-b83f-e6887c0575d0","feature":"Menubar Feature","scenario":"Validation of manubar","start":1663213610351,"group":1,"content":"","tags":"","end":1663213657543,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
-});
